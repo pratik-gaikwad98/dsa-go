f84c05707dd68aa9335cda9bcb6b3543144d19c1
pratik-gaikwad98 <pratik.gaikwad@merce.co>
2024-01-14 12:34:13 +0530

    refactored code bigO

--- bigO/bigO.go
+++ bigO/bigO.go
@@ -6,7 +6,7 @@ import "fmt"
 
 func CalculateBigO() {
 	LinearTime()
-
+	ConstantTime()
 }
 
 // O(n) -> Linear Time(Increases time according to iteration)
@@ -14,6 +14,7 @@ func CalculateBigO() {
 //  2. if the loop size is 10 and 30 element is on 10 so the loop will iteterate 10 times
 //     iteration depend on length of array thats y it is O(n) >> n times loop will iterate
 func LinearTime() {
+	fmt.Println("\nLinear Time [O(n)]")
 	myArray := []int{10, 20, 30}
 	find30 := 30
 	for location, value := range myArray {
@@ -21,4 +22,14 @@ func LinearTime() {
 			fmt.Printf("Found on Location: %v & Value is : %d\n", location, value)
 		}
 	}
+}
+
+// O(n) -> Constant Time(Increases time according to iteration)
+//  1. we know the location of element so we can directly print the element
+//
+// it only take 1 time to find so its constant
+func ConstantTime() {
+	fmt.Println("\nConstant Time [O(1)]")
+	myArray := []int{10, 20, 30}
+	fmt.Printf("Found on Location: %v & Value is : %d\n", 2, myArray[2])
 }
